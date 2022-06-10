95f86d97e18a237e36c80d3d6c0a7fad76074b6a
simonkim1 <sevencuter@gmail.com>
2022-06-10 16:20:33 +0900

    node.js make timer

--- event_basic/custom_module_timer.js
+++ event_basic/custom_module_timer.js
@@ -1,8 +1,7 @@
 const EventEmitter = require("events");
-const sec = 1; // setInterval 함수가 동작하는 interval 값을 설명한다. -> 1초에 한번씩 호출
 
 exports.timer = new EventEmitter(); // timer 변수를 EventEmitter 로 초기화
 
 setInterval(() => {
     exports.timer.emit("tick");
-}, sec * 1000);
+}, 1000);
